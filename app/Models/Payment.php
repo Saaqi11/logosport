6a85fb61a4a88c793728c58ff0fbfca075d1b018
SaaqiAli <alihassna622551@gmail.com>
2024-03-13 21:23:44 +0500

    payment model fillable

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
+
+    protected $fillable = 
+        [
+            'contest_id',
+            'user_id',
+            'amount',
+            'create_date',
+            'currency',
+            'status',
+            'method',
+            'order_id',
+            'payment_id',
+        ];
 }
